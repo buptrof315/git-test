83d32a245e6dd87600c5c3409f5e5d320301b550
Lipeng <Lipeng@bupt.edu.cn>
2021-08-22 17:14:42 +0800

    third commit

--- src/main/java/com/atguigu/boot/listener/MyCommandLineRunner.java
+++ src/main/java/com/atguigu/boot/listener/MyCommandLineRunner.java
@@ -9,5 +9,6 @@ public class MyCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("MyCommandLineRunner....run...");
+        System.out.println("Heloo git 3");
     }
 }
